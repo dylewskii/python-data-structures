fea5668e7de4502b7829961128d56318294ee49c
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-01-12 17:08:55 +0000

    add bfs

--- bst.py
+++ bst.py
@@ -94,7 +94,7 @@ class BinarySearchTree:
 			    current_node = current_node.left
 		    else:
 			    sub_tree_min = self.min_value(current_node.right)
-			    current_node.value = sub_tree_min
+                current_node.value = sub_tree_min
 			    current_node.right = self.__delete_node(current_node.right, sub_tree_min)
 	    return current_node
     
